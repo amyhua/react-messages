cfed85f544ceb549db1c841663db96a42da5ae8d
amyhua <amy@hua.ninja>
2016-09-27 21:26:59 -0400

    Create component hierarchy, folders, initial App component imports

---
+++ src/components/App/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import MessageCount from '../MessageCount/MessageCount';
+import MessageFilter from '../MessageFilter/MessageFilter';
+import MessageList from '../MessageList/MessageList';
+
+/*
+
+- Inbox of messages
+- Filter the messages by Read, Unread, All
+- Mark each individual message as "Read"
+
+1. What are our components?
+2. What is the component hierarchy?
+App
+  MessageList
+    []Message
+      MessageText
+      ReadCheckbox
+  MessageFilter
+  MessageCount
+3. Static representation of your App (nondynamic)
+
+*/
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <MessageCount />
+          <MessageFilter />
+        </div>
+        <MessageList />
+      </div>
+    );
+  }
+}
+
+export default App;
